b605433c4cef01da68444f74c61717bf2208c2c6
diegovoos <d.vigneron@gmail.com>
2022-12-23 17:13:11 +0100

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ClubPenguinAuto
 ClubPenguin scripts to automate certain activities and AFK them
+
+#clubPenguinAutoMine
+It's a script meant to be used inside the mine. Once you're there run the
+script and it will tell you to select three different mining spots, the
+puffle action menu and the puffle money bag icon inside the menu. When
+all of those have been saved, the script will start running and you can
+stay AFK
---
+++ clubPenguinAutoMine.py
@@ -0,0 +1,104 @@
+import time
+import keyboard
+from pynput.mouse import Button, Controller
+import os
+
+mouse = Controller()
+
+os.system("clear")
+
+print('Put your mouse in the spot you want to mine')
+print('Saving spot #1 in 3 seconds...')
+time.sleep(3)
+pos_one = mouse.position
+print('Saved #1')
+
+
+print('Put your mouse in the spot you want to mine')
+print(' ')
+print('Saving spot #2 in 3 seconds...')
+time.sleep(3)
+pos_two = mouse.position
+print('Saved #2')
+
+print('Put your mouse in the spot you want to mine')
+print(' ')
+print('Saving spot #3 in 3 seconds...')
+time.sleep(3)
+pos_three = mouse.position
+print('Saved #3')
+time.sleep(1)
+
+print('Put your mouse in the puffle action menu')
+print(' ')
+print('Saving in 3 seconds...')
+time.sleep(3)
+pos_puffle_action_menu = mouse.position
+mouse.click(Button.left,1)
+print('Saved puffle action menu')
+time.sleep(1)
+print(' ')
+
+print('Put your mouse in the money bag icon from the puffle menu')
+print(' ')
+print('Saving in 3 seconds...')
+time.sleep(3)
+pos_puffle_action_button = mouse.position
+print('Saved puffle action money bag')
+
+print(' ')
+
+print('Starting...')
+
+os.system('clear')
+
+puffle_action_time = 0
+
+def puffle_action():
+    print('puffle mining action running')
+    mouse.position = pos_puffle_action_menu
+    time.sleep(1)
+    mouse.click(Button.left,1)
+    mouse.position = pos_puffle_action_button
+    time.sleep(1)
+    mouse.click(Button.left,1)
+
+def run():
+    global puffle_action_time
+    global mouse
+
+    if puffle_action_time == 4:
+        puffle_action_time = 0
+        puffle_action()
+        os.system('clear')
+
+    time.sleep(9)
+    print('pos #1')
+    mouse.position = pos_one
+    #time.sleep(1)
+    mouse.click(Button.left,1)
+    time.sleep(2)
+    keyboard.press_and_release('d')
+
+    time.sleep(9)
+    print('pos #2')
+    mouse.position = pos_two
+    #time.sleep(1)
+    mouse.click(Button.left,1)
+    time.sleep(2)
+    keyboard.press_and_release('d')
+
+    time.sleep(9)
+    print('pos #3')
+    mouse.position = pos_three
+    #time.sleep(1)
+    mouse.click(Button.left,1)
+    time.sleep(2)
+    keyboard.press_and_release('d')
+
+    puffle_action_time += 1
+    print('puffle action time is:', puffle_action_time)
+
+while True:
+    run()
+    
